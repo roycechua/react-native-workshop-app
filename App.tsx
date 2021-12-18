55bfc831d093cf949e3f932a796e88004e796fdd
roycechua23 <royce236@gmail.com>
2021-12-18 17:40:30 +0800

    Add Create, Delete Functionality and Redux

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import MainNavigationContainer from './src/navigation/MainNavigationContainer';
+import { store } from './src/redux/store';
 
 export default function App () {
   return (
-    <MainNavigationContainer/>
+    <Provider store={store}>
+        <MainNavigationContainer/>
+    </Provider>
   );
 }
